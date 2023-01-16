be001a47efa40795727d95d3a59cacc3bafad922
Fabio Plunser <f.plunser@outlook.com>
2023-01-16 20:40:21 +0100

    added sheet_10

---
+++ sheet_10/2a.sql
@@ -0,0 +1,7 @@
+SELECT first_name, last_name, count(film_id)
+FROM film_actor
+INNER JOIN actor
+ON actor.actor_id = film_actor.actor_id
+WHERE film_id != ALL(SELECT film_id FROM film WHERE length < 120)
+GROUP BY first_name, last_name
+ORDER BY first_name, last_name
---
+++ sheet_10/2b.sql
@@ -0,0 +1,5 @@
+SELECT          customer_id,
+                last_name,
+                first_name
+FROM            customer
+ORDER BY        last_name ASC
